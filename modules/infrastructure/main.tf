44d67420f411849fb6cffecded0a6f738586229e
dancaps <danny.caperton@gmail.com>
2022-12-06 16:44:36 +0000

    FIX: incorrect variable name

--- modules/infrastructure/main.tf
+++ modules/infrastructure/main.tf
@@ -21,7 +21,7 @@ resource "google_compute_instance" "vm_instance" {
 
 # Network Resources
 resource "google_compute_network" "vpc_network" {
-  name                    = var.network_name
+  name                    = var.vpc_name
   auto_create_subnetworks = false
   mtu                     = 1460
 }
